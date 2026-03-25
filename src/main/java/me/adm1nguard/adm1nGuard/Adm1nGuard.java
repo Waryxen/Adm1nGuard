3ba42b1a3dd1c60b2b67b0e7d831abc7c77259f8
Waryxen <Waryxen@proton.me>
2026-03-24 20:19:19 -0500

    Initial plugin commit

---
+++ src/main/java/me/adm1nguard/adm1nGuard/Adm1nGuard.java
@@ -0,0 +1,17 @@
+package me.adm1nguard.adm1nGuard;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Adm1nGuard extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
